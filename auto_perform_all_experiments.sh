3e8a5e0dc2e27e46af46ddcf71ab39d19a315afd
koya <koya@dsl.cs.tut.ac.jp>
2022-07-04 21:06:50 +0900

    remove start & stop

--- auto_perform_all_experiments.sh
+++ auto_perform_all_experiments.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "StartTime -----> `date`"
-./start_instances_on_all_regions.sh
 ./perform_throughput_experiments.sh >> throughput.log&
 ./perform_rtt_experiments.sh >> rtt.log&
-./stop_instances_on_all_regions.sh
 echo "EndTime -----> `date`"
