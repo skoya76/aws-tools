09f2592940fbc0dc22f24b270fb2212d79a5a22f
koya <koya@dsl.cs.tut.ac.jp>
2022-07-04 21:08:35 +0900

    fix

--- auto_perform_all_experiments.sh
+++ auto_perform_all_experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "StartTime -----> `date`"
-./perform_throughput_experiments.sh >> throughput.log&
-./perform_rtt_experiments.sh >> rtt.log&
+./perform_throughput_experiments.sh >> throughput.log &
+./perform_rtt_experiments.sh >> rtt.log &
 echo "EndTime -----> `date`"
